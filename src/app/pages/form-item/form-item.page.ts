2bd4b2a3a9f458a27646b060e715dbd978707f78
FilipeSouza <filipe.souza1602@gmail.com>
2023-01-08 20:32:57 -0300

    feat: get localization from service geolocation

--- src/app/pages/form-item/form-item.page.ts
+++ src/app/pages/form-item/form-item.page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
-import { Location } from "@angular/common";
+import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ReactiveFormItemCreate } from './form-item-create.interface';
+import { Localization, ReactiveFormItemCreate } from './form-item-create.interface';
+import { GeolocationService } from 'src/app/shared/services/geolocation/geolocation.service';
 
 @Component({
   selector: 'app-form-item',
@@ -11,28 +12,67 @@ import { ReactiveFormItemCreate } from './form-item-create.interface';
 })
 export class FormItemPage {
   public form: FormGroup<ReactiveFormItemCreate>;
+  public localization: Localization = {
+    longitude: '',
+    latitude: ''
+  };
+  public isLocalizationFilled = false
 
   constructor(
     private platform: Platform,
     private location: Location,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private geolocationService: GeolocationService,
   ) {
     this.platform.backButton.subscribeWithPriority(10, () => {
-      this.backToPreviousPage()
+      this.backToPreviousPage();
     });
 
     this.form = this.formBuilder.group({
       name: ['', Validators.required],
       image: ['', Validators.required],
-      localization: ['', Validators.required],
+      localizationLongitude: ['' , Validators.required],
+      localizationLatitude: ['' , Validators.required],
     });
-  }
 
-  submit(): void {
-    console.log('this.form.controls: ',this.form.controls);
+    this.updateLocalization(this.form)
   }
 
   backToPreviousPage(): void {
     this.location.back();
+  }
+
+  updateLocalization(form: FormGroup<ReactiveFormItemCreate>): void {
+    if (form.controls.localizationLatitude.value && form.controls.localizationLongitude.value) {
+      const localization = this.createLocalizationFromFormControls(form);
+      this.localization = localization;
+      this.isLocalizationFilled = true;
+    }
+  }
+
+  createLocalizationFromFormControls({ controls }: FormGroup<ReactiveFormItemCreate>): Localization {
+    return {
+      latitude: controls.localizationLatitude.value!.toString(),
+      longitude: controls.localizationLongitude.value!.toString(),
+    };
+  }
+
+  setReactiveFormLocalization(localization: Localization): void {
+    this.form.controls.localizationLatitude.setValue(localization.latitude)
+    this.form.controls.localizationLongitude.setValue(localization.longitude)
+  }
+
+  async getCurrentLocalization(): Promise<void> {
+    const newLocalization = await this.geolocationService.getCurrentLocalization()
+
+    if(newLocalization) {
+      this.isLocalizationFilled = true
+      this.localization = newLocalization;
+      this.setReactiveFormLocalization(newLocalization);
+    }
+  }
+
+  submit(): void {
+    console.log('this.form.controls: ', this.form.controls);
   }
 }
