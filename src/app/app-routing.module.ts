08cd872ae6c2e5a6bca74d999bd3bbdefb5779fa
FilipeSouza <filipe.souza1602@gmail.com>
2023-01-08 12:48:45 -0300

    feat: create pages module with list items page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { ListItemsPage } from './pages/list-items/list-items.page';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'list-items',
     pathMatch: 'full'
   },
+  {
+    path: 'list-items',
+    component: ListItemsPage
+    // loadChildren: () => import('./pages/list-items/list-items.module').then( m => m.ListItemsPageModule)
+  }
 ];
 
 @NgModule({
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { ListItemsPageModule } from "./list-items/list-items.module";
+
+@NgModule({
+  imports: [ListItemsPageModule]
+})
+
+export class PagesModule {}
