24be184ad893c107509e7ac41ccccc8604b64b75
FilipeSouza <filipe.souza1602@gmail.com>
2023-01-08 18:02:54 -0300

    feat: initialize form builder group

--- src/app/pages/form-item/form-item.page.ts
+++ src/app/pages/form-item/form-item.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormItemCreate } from './form-item-create.interface';
 
 @Component({
   selector: 'app-form-item',
   templateUrl: './form-item.page.html',
   styleUrls: ['./form-item.page.scss'],
 })
-export class FormItemPage implements OnInit {
+export class FormItemPage {
+  public form: FormGroup<ReactiveFormItemCreate>;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
 
-  ngOnInit() {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      image: ['', Validators.required],
+      localization: ['', Validators.required],
+    });
+  }
+
+  submit(): void {
+    console.log('this.form.controls: ',this.form.controls);
   }
 
 }
