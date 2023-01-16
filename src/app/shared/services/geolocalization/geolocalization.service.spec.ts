a3231e4a37e8fa580ea2cb1cdf9e9d03f5c77518
FilipeSouza <filipe.souza1602@gmail.com>
2023-01-16 20:29:42 -0300

    refactor: simplify geolocation test

--- src/app/shared/services/geolocalization/geolocalization.service.spec.ts
+++ src/app/shared/services/geolocalization/geolocalization.service.spec.ts
@@ -1,24 +1,14 @@
-import { Position } from '@capacitor/geolocation';
-import { Localization } from 'src/app/pages/form-item/form-item-create.interface';
+import { makeGeolocalizationPositionMock, makeLocalizationMock } from 'src/tests/factories/geolocalization-factory';
 import { InMemoryGeolocalizationRepository } from 'src/tests/repositories/in-memory-geolocalization-repository';
 import { GeolocalizationService } from './geolocalization.service'
 
-const currentLocalizationMock: Localization = {
-  latitude: '150',
-  longitude: '250'
-}
+const currentLocalizationMock = makeLocalizationMock()
 
-function initializeGeolocalizationService(): GeolocalizationService {
-  const geolocationRepository = new InMemoryGeolocalizationRepository()
-  const coords = geolocationRepository.coords
+function initializeServiceWithLocalizationMock(): GeolocalizationService {
+  const geolocalizationRepository = new InMemoryGeolocalizationRepository()
+  geolocalizationRepository.setLocalizationMockToCoords(currentLocalizationMock)
 
-  geolocationRepository.coords = {
-    ...coords,
-    latitude: +currentLocalizationMock.latitude,
-    longitude: +currentLocalizationMock.longitude
-  }
-
-  const geolocationService = new GeolocalizationService(geolocationRepository)
+  const geolocationService = new GeolocalizationService(geolocalizationRepository)
   return geolocationService
 }
 
@@ -26,24 +16,12 @@ describe("Geolocation Service", () => {
   let geolocationService: GeolocalizationService
 
   beforeEach(() => {
-    geolocationService = initializeGeolocalizationService()
+    geolocationService = initializeServiceWithLocalizationMock()
   })
 
   it("should return a new localization object from coordinates", async () => {
-    const coordinates: Position = {
-      timestamp: 0,
-      coords: {
-        latitude: 150,
-        longitude: 250,
-        accuracy: 0,
-        altitude: 0,
-        altitudeAccuracy: 0,
-        heading: 0,
-        speed: 0
-      }
-    }
-
-    const newLocalization = geolocationService.createLocalizationFromCoordinates(coordinates);
+    const positionCoordinates = makeGeolocalizationPositionMock()
+    const newLocalization = geolocationService.createLocalizationFromCoordinates(positionCoordinates);
 
     expect(newLocalization).toBeTruthy();
     expect(newLocalization).toEqual(currentLocalizationMock);
