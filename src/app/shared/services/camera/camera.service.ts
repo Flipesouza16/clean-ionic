3cc5c11604f1dc194153754cc8a7b7d38fe7cf80
FilipeSouza <filipe.souza1602@gmail.com>
2023-01-22 20:06:30 -0300

    feat: create camera service

---
+++ src/app/shared/services/camera/camera.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { CameraRepository } from 'src/app/repositories/camera-repository';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CameraService {
+
+  constructor(private cameraRepository: CameraRepository) { }
+
+  async openCamera(): Promise<Photo> {
+    try {
+      const photo = await this.cameraRepository.openCamera()
+      return photo
+    } catch(error) {
+      console.error(error)
+      throw error
+    }
+  }
+}
